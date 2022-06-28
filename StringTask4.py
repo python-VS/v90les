9b8d50bf5c6dd02a67737a56c81a4fbdbb09c8b7
vitali_saharov <vitali.saharov@gmail.com>
2022-06-29 01:58:29 +0300

    написан код 4 задания

--- StringTask4.py
+++ StringTask4.py
@@ -1,3 +1,7 @@
-a=input('Введите строку:')
-b=input('Введите символ:')
-print(a.lower().count(b))
+c=input('Введите строку:')
+d=input('Введите символ:')
+if d in c:
+    print(c.find(d))
+    print(c.rfind(d))
+else:
+    print("заданный символ не содержится в введенной строке!")
